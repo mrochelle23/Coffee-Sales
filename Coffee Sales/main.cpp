b27f96821fee8cbfc1ea17781d32cdfd82856997
mrochelle23 <mrochellebiz@gmail.com>
2024-05-19 22:42:44 -0400

    Initial Commit

---
+++ Coffee Sales/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Coffee Sales
+//
+//  Created by Mikhai Rochelle on 5/19/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
